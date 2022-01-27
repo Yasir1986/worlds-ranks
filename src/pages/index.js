ef8c807b4a2d012c7a82f74496289a37feeeaec4
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-27 20:58:21 +0000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout/Layout"
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+  return (
+    <div>
+  <Layout>main</Layout>
+  </div>
+  );
+}
