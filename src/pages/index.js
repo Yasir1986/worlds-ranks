62adf43590eb7aed9017ff2a557902e0764963a2
Yasir1986 <yasir.akbar86@hotmail.com>
2022-01-28 21:04:33 +0000

    search input added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,28 @@
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 import Layout from "../components/Layout/Layout"
+import SearchInput from "../components/SearchInput/SearchInput"
 import styles from "../styles/Home.module.css"
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return (
     <div>
-  <Layout>main</Layout>
-  </div>
+      <Layout>
+        <div className={styles.counts}>Found {countries.length} countries</div>
+        <SearchInput placeholder="Filter By Name, Region or SubRegion" />
+        <CountriesTable countries={countries} />
+      </Layout>
+    </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.com/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
